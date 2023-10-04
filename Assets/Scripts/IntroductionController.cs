ebc7f9300dec862f741f47958847fe17910b6836
odafenix <68091954+odafenix@users.noreply.github.com>
2023-10-03 22:58:35 -0300

    Orden de paneles en codigo de introducción

--- Assets/Scripts/IntroductionController.cs
+++ Assets/Scripts/IntroductionController.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 
 public class IntroductionController : MonoBehaviour
 {
-    public int currentPage;
-    public int currentDrawings;
+    public float currentPage;
+    public float currentDrawings;
     public GameSceneManager gsm;
     
     [Header("Drawings")]
     public GameObject drawing01;
     public GameObject drawing02;
+    public GameObject drawingtext01;
     public GameObject drawing03;
     public GameObject drawing04;
     public GameObject drawing05;
@@ -26,7 +27,7 @@ public class IntroductionController : MonoBehaviour
     public GameObject drawing14;
     public GameObject drawing15;
     public GameObject drawing16;
-    public GameObject drawing17;
+    //public GameObject drawing17;
     public GameObject drawing18;
     public GameObject drawing19;
     public GameObject drawing20;
@@ -36,8 +37,16 @@ public class IntroductionController : MonoBehaviour
     public GameObject drawing24;
     public GameObject drawing25;
     public GameObject drawing26;
+    public GameObject drawing27;
+    public GameObject drawing28;
+    public GameObject drawing29;
+    public GameObject drawing30;
+    public GameObject drawing31;
+    public GameObject drawing32;
+    public GameObject drawing33;
 
-    [Header("Intro Texts")]
+    /*
+    [Header("Texts")]
     public GameObject introText01;
     public GameObject introText02;
     public GameObject introText03;
@@ -54,272 +63,227 @@ public class IntroductionController : MonoBehaviour
     public GameObject introText14;
     public GameObject introText15;
     public GameObject introText16;
-
+    */
 
     public void ChangeDrawing ()
     {
-       if (currentPage == 1)
-       {
-            if(currentDrawings == 0)
-            {
-                drawing01.SetActive(true);
-                introText01.SetActive(true);
-                currentDrawings = 1;
-                return;
-            }
-            if(currentDrawings == 1)
-            {
-                drawing02.SetActive(true);
-                introText01.SetActive(false);
-                currentDrawings = 2;
-                return;
-            }
-            if(currentDrawings == 2)
-            {
-                drawing03.SetActive(true);
-                introText02.SetActive(true);
-                currentDrawings = 3;
-                return;
-            }
-            if(currentDrawings == 3)
-            {
-                drawing04.SetActive(true);
-                introText02.SetActive(false);
-                introText03.SetActive(true);
-                currentDrawings = 4;
-                return;
-            }
-            if(currentDrawings == 4)
-            {
-                drawing01.SetActive(false);
-                drawing02.SetActive(false);
-                drawing03.SetActive(false);
-                drawing04.SetActive(false);
-                introText03.SetActive(false);
-                currentDrawings = 0;
-                currentPage = 2;
-                return;
-            }
-       }
+    
+        if(currentDrawings == 0)
+        {
+            drawing01.SetActive(true);
+            currentDrawings = 1;
+            return;
+        }
+        if(currentDrawings == 1)
+        {
+            drawing02.SetActive(true);
+            currentDrawings = 1.5f;
+            return;
+        }
+        
+        if(currentDrawings == 1.5f)
+        {
+            drawingtext01.SetActive(true);
+            currentDrawings = 2;
+            return;
+        }
 
-       if (currentPage == 2)
-       {
-            if(currentDrawings == 0)
-            {
-                drawing05.SetActive(true);
-                drawing06.SetActive(true);
-                introText04.SetActive(true);
-                currentDrawings = 1;
-                return;
-            }
-            if(currentDrawings == 1)
-            {
-                
-                drawing05.SetActive(false);
-                drawing06.SetActive(false);
-                introText04.SetActive(false);
-                currentDrawings = 0;
-                currentPage = 3;
-                return;
-            }
-            
-       }
+        if(currentDrawings == 2)
+        {
+            drawing03.SetActive(true);
+            currentDrawings = 3;
+            return;
+        }
+        if(currentDrawings == 3)
+        {
+            drawing04.SetActive(true);
+            currentDrawings = 4;
+            return;
+        }
+        if(currentDrawings == 4)
+        {
+            drawing05.SetActive(true);
+            currentDrawings = 5;
+            return;
+        }
+        if(currentDrawings == 5)
+        {
+            drawing06.SetActive(true);
+            currentDrawings = 6;
+            return;
+        }
+        if(currentDrawings == 6)
+        {
+            drawing07.SetActive(true);
+            currentDrawings = 7;
+            return;
+        }
+        if(currentDrawings == 7)
+        {
+            drawing08.SetActive(true);
+            currentDrawings = 8;
+            return;
+        }
+        if(currentDrawings == 8)
+        {
+            drawing09.SetActive(true);
+            currentDrawings = 9;
+            return;
+        }
+        if(currentDrawings == 9)
+        {
+            drawing10.SetActive(true);
+            currentDrawings = 10;
+            return;
+        }
+        if(currentDrawings == 10)
+        {
+            drawing11.SetActive(true);
+            currentDrawings = 11;
+            return;
+        }
+        if(currentDrawings == 11)
+        {
+            drawing12.SetActive(true);
+            currentDrawings = 12;
+            return;
+        }
+        if(currentDrawings == 12)
+        {
+            drawing13.SetActive(true);
+            currentDrawings = 13;
+            return;
+        }
+        if(currentDrawings == 13)
+        {
+            drawing14.SetActive(true);
+            currentDrawings = 14;
+            return;
+        }
+        if(currentDrawings == 14)
+        {
+            drawing15.SetActive(true);
+            currentDrawings = 15;
+            return;
+        }
+        if(currentDrawings == 15)
+        {
+            drawing16.SetActive(true);
+            currentDrawings = 17;
+            return;
+        }
+        /*if(currentDrawings == 16)
+        {
+            drawing17.SetActive(true);
+            currentDrawings = 17;
+            return;
+        }*/
+        if(currentDrawings == 17)
+        {
+            drawing18.SetActive(true);
+            currentDrawings = 18;
+            return;
+        }
+        if(currentDrawings == 18)
+        {
+            drawing19.SetActive(true);
+            currentDrawings = 19;
+            return;
+        }
+        if(currentDrawings == 19)
+        {
+            drawing20.SetActive(true);
+            currentDrawings = 20;
+            return;
+        }
+        if(currentDrawings == 20)
+        {
+            drawing21.SetActive(true);
+            currentDrawings = 21;
+            return;
+        }
+        if(currentDrawings == 21)
+        {
+            drawing22.SetActive(true);
+            currentDrawings = 22;
+            return;
+        }
+        if(currentDrawings == 22)
+        {
+            drawing23.SetActive(true);
+            currentDrawings = 23;
+            return;
+        }
+        if(currentDrawings == 23)
+        {
+            drawing24.SetActive(true);
+            currentDrawings = 24;
+            return;
+        }
+        if(currentDrawings == 24)
+        {
+            drawing25.SetActive(true);
+            currentDrawings = 25;
+            return;
+        }
+        if(currentDrawings == 25)
+        {
+            drawing26.SetActive(true);
+            currentDrawings = 26;
+            return;
+        }
+        if(currentDrawings == 26)
+        {
+            drawing27.SetActive(true);
+            currentDrawings = 27;
+            return;
+        }
+        if(currentDrawings == 27)
+        {
+            drawing28.SetActive(true);
+            currentDrawings = 28;
+            return;
+        }
+        if(currentDrawings == 28)
+        {
+            drawing29.SetActive(true);
+            currentDrawings = 29;
+            return;
+        }
+        if(currentDrawings == 29)
+        {
+            drawing30.SetActive(true);
+            currentDrawings = 30;
+            return;
+        }
+        if(currentDrawings == 30)
+        {
+            drawing31.SetActive(true);
+            currentDrawings = 31;
+            return;
+        }
+        if(currentDrawings == 31)
+        {
+            drawing32.SetActive(true);
+            currentDrawings = 32;
+            return;
+        }
+        if(currentDrawings == 32)
+        {
+            drawing33.SetActive(true);
+            currentDrawings = 33;
+            return;
+        }
+        if(currentDrawings == 33)
+        {
+            gsm.GoToBriefing();
+            return;
+        }
 
-       if (currentPage == 3)
-       {
-            if(currentDrawings == 0)
-            {
-                drawing07.SetActive(true);
-                drawing08.SetActive(true);
-                introText05.SetActive(true);
-                currentDrawings = 1;
-                return;
-            }
-            if(currentDrawings == 1)
-            {
-                drawing09.SetActive(true);
-                introText05.SetActive(false);
-                introText06.SetActive(true);
-                currentDrawings = 2;
-                return;
-            }
-            if(currentDrawings == 2)
-            {
-                drawing07.SetActive(false);
-                drawing08.SetActive(false);
-                drawing09.SetActive(false);
-                introText06.SetActive(false);
-                currentDrawings = 0;
-                currentPage = 4;
-                return;
-            }
-            
-       }
-
-       if (currentPage == 4)
-       {
-            if(currentDrawings == 0)
-            {
-                drawing10.SetActive(true);
-                introText07.SetActive(true);
-                currentDrawings = 1;
-                return;
-            }
-            if(currentDrawings == 1)
-            {
-                drawing11.SetActive(true);
-                introText07.SetActive(false);
-                currentDrawings = 2;
-                return;
-            }
-            if(currentDrawings == 2)
-            {
-                drawing12.SetActive(true);
-                introText08.SetActive(true);
-                currentDrawings = 3;
-                return;
-            }
-            if(currentDrawings == 3)
-            {
-                drawing10.SetActive(false);
-                drawing11.SetActive(false);
-                drawing12.SetActive(false);
-                introText08.SetActive(false);
-                currentDrawings = 0;
-                currentPage = 5;
-                return;
-            }
-       }
-
-       if (currentPage == 5)
-       {
-            if(currentDrawings == 0)
-            {
-                drawing13.SetActive(true);
-                introText09.SetActive(true);
-                currentDrawings = 1;
-                return;
-            }
-            if(currentDrawings == 1)
-            {
-                drawing13.SetActive(false);
-                introText09.SetActive(false);
-                currentDrawings = 0;
-                currentPage = 6;
-                return;
-            }
-       }
-
-       if (currentPage == 6)
-       {
-            if(currentDrawings == 0)
-            {
-                drawing14.SetActive(true);
-                introText10.SetActive(true);
-                currentDrawings = 1;
-                return;
-            }
-            if(currentDrawings == 1)
-            {
-                drawing14.SetActive(false);
-                introText10.SetActive(false);
-                currentDrawings = 0;
-                currentPage = 7;
-                return;
-            }
-       }
-
-       if (currentPage == 7)
-       {
-            if(currentDrawings == 0)
-            {
-                drawing15.SetActive(true);
-                currentDrawings = 1;
-                return;
-            }
-            if(currentDrawings == 1)
-            {
-                drawing16.SetActive(true);
-                currentDrawings = 2;
-                return;
-            }
-            if(currentDrawings == 2)
-            {
-                drawing17.SetActive(true);
-                currentDrawings = 3;
-                return;
-            }
-            if(currentDrawings == 3)
-            {
-                drawing18.SetActive(true);
-                introText11.SetActive(true);
-                currentDrawings = 4;
-                return;
-            }
-            if(currentDrawings == 4)
-            {
-                drawing19.SetActive(true);
-                introText11.SetActive(false);
-                introText12.SetActive(true);
-                currentDrawings = 5;
-                return;
-            }
-            if(currentDrawings == 5)
-            {
-                drawing15.SetActive(false);
-                drawing16.SetActive(false);
-                drawing17.SetActive(false);
-                drawing18.SetActive(false);
-                drawing19.SetActive(false);
-                introText12.SetActive(false);
-                currentDrawings = 0;
-                currentPage = 8;
-                return;
-            }
-       }
-
-       if (currentPage == 8)
-       {
-            if(currentDrawings == 0)
-            {
-                drawing20.SetActive(true);
-                introText13.SetActive(true);
-                currentDrawings = 1;
-                return;
-            }
-            if(currentDrawings == 1)
-            {
-                drawing21.SetActive(true);
-                drawing22.SetActive(true);
-                introText13.SetActive(false);
-                introText14.SetActive(true);
-                currentDrawings = 2;
-                return;
-            }
-            if(currentDrawings == 2)
-            {
-                drawing23.SetActive(true);
-                drawing24.SetActive(true);
-                introText14.SetActive(false);
-                introText15.SetActive(true);
-                currentDrawings = 3;
-                return;
-            }
-            if(currentDrawings == 3)
-            {
-                drawing25.SetActive(true);
-                drawing26.SetActive(true);
-                introText15.SetActive(false);
-                introText16.SetActive(true);
-                currentDrawings = 4;
-                return;
-            }
-            if(currentDrawings == 4)
-            {
-                gsm.GoToBriefing();
-                return;
-            }
-            
-       }
     }
+
+       
+            
+       
+    
 }
